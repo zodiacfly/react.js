22236f58b1150afedaca3e8114d374248be7f4ce
zodiacfly <renzodiac@hotmail.com>
2019-05-16 21:12:40 +1000

    build add-comment-redux and ready to deploy

---
+++ build-add-comment-redux/precache-manifest.c41c12f08f3d1428e93528b242df43ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b308d9ad587043485782bee05d3821",
+    "url": "./index.html"
+  },
+  {
+    "revision": "361e04028eaa8f31ffff",
+    "url": "./static/css/main.4767428b.chunk.css"
+  },
+  {
+    "revision": "31833904ceac540a248c",
+    "url": "./static/js/2.2da1c5bb.chunk.js"
+  },
+  {
+    "revision": "361e04028eaa8f31ffff",
+    "url": "./static/js/main.23b93b37.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
