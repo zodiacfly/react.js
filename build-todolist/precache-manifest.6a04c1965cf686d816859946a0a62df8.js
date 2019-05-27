d93615aff1c76217885397b86803f8746f14a39d
zodiacfly <renzodiac@hotmail.com>
2019-05-27 11:27:53 +1000

    build todolist

---
+++ build-todolist/precache-manifest.6a04c1965cf686d816859946a0a62df8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532caa82346bd17bfadfe4c7d3116cae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49bdd38bb875caa6417b",
+    "url": "./static/js/2.b7455171.chunk.js"
+  },
+  {
+    "revision": "ecbebdfb60eadc34d961",
+    "url": "./static/js/main.afcbab15.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
